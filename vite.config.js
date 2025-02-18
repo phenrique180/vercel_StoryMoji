13abf44079f57e0963f97b38c3e08a615fff7849
phenrique180 <acessonetph@gmail.com>
2025-02-18 17:06:29 -0300

    emoji pika

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'EmojiPika',
+        short_name: '***NomeCurto',
+        description: 'EmojiPika',
+        theme_color: '***#ffffff',
+        icons: [
+          {
+            src: 'moji192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'moji512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
